a6663a15cf250c3159d37c7553821d30371150e7
Valery <drozdllera@gmail.com>
2021-03-03 20:55:35 +0200

    Testing new config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return <div></div>;
+function App() 
+
+{
+  return             (
+    <div>
+      <div></div>
+    </div>
+  );
 }
 
 export default App;
