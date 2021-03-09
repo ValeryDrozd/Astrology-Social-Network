5abe479becb576292de1d4f9aa27b42ce9682f65
ValeryDrozd <drozd.ione@gmail.com>
2021-03-09 22:53:04 +0200

    add .env and config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import 'dotenv/config';
+
 async function bootstrap(): Promise<void> {
+  const port = process.env.PORT ? process.env.PORT : 3001;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
