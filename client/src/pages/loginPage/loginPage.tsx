e3896d8c65242556e1e08afd1bff9e086d156fb2
ValeryDrozd <drozd.ione@gmail.com>
2021-03-18 00:58:08 +0200

    Add styled components

--- client/src/pages/loginPage/loginPage.tsx
+++ client/src/pages/loginPage/loginPage.tsx
@@ -1,28 +1,30 @@
-import './loginPage.css';
+import {
+  LoginForm,
+  LoginDiv,
+  ButtonBox,
+  LoginInput,
+  StyledButton,
+  Title,
+} from './styles';
 
-function LoginPage(): JSX.Element {
+export default function LoginPage(): JSX.Element {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+  };
   return (
-    <div>
-      <div className="login-div">
-        <form className="login-form">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" />
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
-          <div className="button-box">
-            <button>Sign in</button>
-            <button>Sign in with GOOGLE</button>
-          </div>
-        </form>
-        <div className="button-box">
-          <button>Sign up</button>
-          <button>Sign up with GOOGLE</button>
-        </div>
-        <p>Login page</p>
-      </div>
-      <div></div>
-    </div>
+    <LoginDiv>
+      <Title>Login</Title>
+      <LoginForm onSubmit={handleSubmit}>
+        <label htmlFor="email">Email</label>
+        <LoginInput type="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <LoginInput type="password" id="password" />
+        <ButtonBox>
+          <StyledButton>Sign in</StyledButton>
+          <StyledButton>Sign in with GOOGLE</StyledButton>
+        </ButtonBox>
+      </LoginForm>
+      <StyledButton>Sign up</StyledButton>
+    </LoginDiv>
   );
 }
-
-export default LoginPage;
