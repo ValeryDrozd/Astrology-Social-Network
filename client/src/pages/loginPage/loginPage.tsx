4e6382157f4494c204ee40fec848b5388f03705f
ValeryDrozd <drozd.ione@gmail.com>
2021-03-17 23:33:15 +0200

    Add Login Form

--- client/src/pages/loginPage/loginPage.tsx
+++ client/src/pages/loginPage/loginPage.tsx
@@ -3,7 +3,21 @@ import './loginPage.css';
 function LoginPage(): JSX.Element {
   return (
     <div>
-      <div>
+      <div className="login-div">
+        <form className="login-form">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" />
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+          <div className="button-box">
+            <button>Sign in</button>
+            <button>Sign in with GOOGLE</button>
+          </div>
+        </form>
+        <div className="button-box">
+          <button>Sign up</button>
+          <button>Sign up with GOOGLE</button>
+        </div>
         <p>Login page</p>
       </div>
       <div></div>
