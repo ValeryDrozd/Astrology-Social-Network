837f21f067c1f37fb100e69a98e8d19470357d05
Michael Medvediev <misha.medvedev2001@gmail.com>
2021-03-24 21:00:02 +0200

    some improvements in sessions saving

--- server/src/modules/messages/messages.gateway.ts
+++ server/src/modules/messages/messages.gateway.ts
@@ -19,20 +19,45 @@ import {
   NewMessageNotification,
   NewMessageNotificationParams,
 } from '../../../../interfaces/rpc-events';
+import { IncomingMessage } from 'http';
+import * as cookie from 'cookie';
 
-@WebSocketGateway()
+interface MessagesSession {
+  socket: WebSocket;
+  token: string;
+}
+
+@WebSocketGateway({ path: '/messages' })
 export class MessagesGateway implements OnGatewayConnection, OnGatewayDisconnect {
   @WebSocketServer()
   server!: Server;
 
+  private clients: MessagesSession[] = [];
+
   constructor(private messagesService: MessagesService) {}
 
-  handleConnection(client: WebSocket): void {
-    this.server.clients.add(client);
+  handleConnection(client: WebSocket, request: IncomingMessage): void {
+    const { cookie: requestCookie } = request.headers;
+    const token = cookie.parse(requestCookie ? requestCookie : '').token;
+
+    if (!requestCookie || !token) {
+      return client.close(1014);
+    }
+
+    const session: MessagesSession = {
+      token,
+      socket: client,
+    };
+
+    this.clients.push(session);
   }
 
   handleDisconnect(client: WebSocket): void {
-    this.server.clients.delete(client);
+    this.clients.splice(
+      this.clients.findIndex((c) => c.socket === client),
+      1,
+    );
+    console.log(this.clients)
     broadcast(this.server, 'disconnect');
   }
 
