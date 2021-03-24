fcd74ba2e3ac57b89ede56e6bd262cdc3cadb185
ValeryDrozd <drozd.ione@gmail.com>
2021-03-24 21:45:33 +0200

    modified mobx to work

--- client/src/pages/chatPage/chatPage.tsx
+++ client/src/pages/chatPage/chatPage.tsx
@@ -4,46 +4,14 @@ import React from 'react';
 import { createRpcConnection } from '../../socket';
 import ChatStore from '../../stores/store';
 
-interface Message {
-  text: string;
-  time: string;
-  id: string;
-}
-
-async function sendMessage(): Promise<void> {
-  const message = {
-    text: 'asadasd',
-    time: new Date().toLocaleTimeString(),
-  };
-  const socket = await createRpcConnection();
-  socket.call<{ ok: boolean }>('addNewMessage', message);
-}
-// setInterval(sendMessage, 60000);
-
 class ChatPage extends React.Component<{ store: ChatStore }> {
-  state: { messages: { text: string }[] } = {
-    messages: [],
-  };
-
-  async componentDidMount(): Promise<void> {
-    // console.log('laslkdlasdklasdk');
-    // const rpcConnection = await createRpcConnection();
-    // console.log(await rpcConnection.call('getMessages'));
-    // const messages = await rpcConnection.call('getMessages');
-    // console.log(await rpcConnection.call('getMessages'));
-    // this.setState({ messages });
-    // console.log('laslkdlasdklasdk');
-  }
-
   render(): JSX.Element {
     const { chats } = this.props.store;
-    const { messages } = this.state;
 
     const messageViews = chats?.length
-      ? chats[0].messageList.map((m) => <p>{m.text}</p>)
+      ? chats[0].messageList.map((m, index) => <p key={index}>{m.text}</p>)
       : [];
 
-    console.log(this.props.store.number);
     return (
       <div>
         <div>
