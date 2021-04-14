ac4b464c0f178e5137eeb6c3a54edc9569bba81d
ValeryDrozd <drozd.ione@gmail.com>
2021-04-14 22:58:16 +0300

    added page for extra information

---
+++ client/src/pages/extraInfoPage/extraInfoPage.tsx
@@ -0,0 +1,9 @@
+import { ExtraInfoDiv, Title } from "./styles";
+
+export default function ExtraInfoPage(): JSX.Element {
+ return(
+     <ExtraInfoDiv>
+         <Title>Enter more information</Title>
+     </ExtraInfoDiv>
+ )
+}
---
+++ client/src/pages/extraInfoPage/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const ExtraInfoDiv = styled.div`
+ margin: 0 auto;
+  padding: 1rem 5rem;
+  display: flex;
+  flex-direction: column;
+`
+
+export const Title = styled.h1`
+text-align: center;
+font-size: calc(3vw + 2rem);
+margin: 1rem;
+`;
